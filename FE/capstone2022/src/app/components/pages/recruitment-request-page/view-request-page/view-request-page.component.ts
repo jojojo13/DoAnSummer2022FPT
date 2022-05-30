c9e8667bf3894706277e2e32e0527ad45feac26e
jojojo13 <longduonghai13@gmail.com>
2022-05-30 22:15:10 +0700

    create recruitment-request page

---
+++ FE/capstone2022/src/app/components/pages/recruitment-request-page/view-request-page/view-request-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-request-page',
+  templateUrl: './view-request-page.component.html',
+  styleUrls: ['./view-request-page.component.scss']
+})
+export class ViewRequestPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
