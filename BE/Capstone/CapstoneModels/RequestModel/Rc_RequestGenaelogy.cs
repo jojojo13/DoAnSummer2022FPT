d493c440d9acb03ade997de874cbb7c99e9057d0
hung nguyenxuan <hung6nuh@gmail.com>
2022-06-08 14:09:26 +0700

    HUNGNX

---
+++ BE/Capstone/CapstoneModels/RequestModel/Rc_RequestGenaelogy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapstoneModels
+{
+    [Table("Rc_RequestGenaelogy")]
+    public class Rc_RequestGenaelogy
+    {
+        [Key]
+        public int Id { get; set; }
+        public int? ParentID { get; set; }
+        [StringLength(100)]
+        public string GenealogyData { get; set; }
+
+        [StringLength(100)]
+        public string CreateBy { get; set; }
+        public DateTime? CreateDate { get; set; }
+        [StringLength(100)]
+        public string UpdateBy { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+}
