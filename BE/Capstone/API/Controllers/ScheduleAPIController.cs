bb8706482652eb0d88c0d368efa6a99598f9d674
hung nguyenxuan <hung6nuh@gmail.com>
2022-07-25 22:04:03 +0700

    ABC

--- BE/Capstone/API/Controllers/ScheduleAPIController.cs
+++ BE/Capstone/API/Controllers/ScheduleAPIController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using API.ResponseModel.Schedule;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ModelAuto.Models;
+using Services.ScheduleServices;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +13,193 @@ namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ScheduleAPIController : ControllerBase
+    public class ScheduleAPIController : AuthorizeByIDController
     {
+        private ISchedule schedule = new ScheduleImpl();
+        #region "Schedule"
+
+        #endregion
+
+
+
+        #region "Thiêt lập môn thi"
+        [Authorize]
+        [HttpPut("ActiveExam")]
+        public IActionResult ActiveExam(List<int> listID)
+        {
+            try
+            {
+                var check = schedule.ActiveExam(listID);
+                if (check)
+                {
+                    return Ok(new
+                    {
+                        Status = true
+                    });
+                }
+                else
+                {
+                    return Ok(new
+                    {
+                        Status = false
+                    });
+                }
+            }
+            catch
+            {
+                return Ok(new
+                {
+                    Status = false
+                });
+            }
+        }
+
+        [Authorize]
+        [HttpPut("DeleteExam")]
+        public IActionResult DeleteExam(List<int> listID)
+        {
+            try
+            {
+                var check = schedule.DeleteExam(listID);
+                if (check)
+                {
+                    return Ok(new
+                    {
+                        Status = true
+                    });
+                }
+                else
+                {
+                    return Ok(new
+                    {
+                        Status = false
+                    });
+                }
+            }
+            catch
+            {
+                return Ok(new
+                {
+                    Status = false
+                });
+            }
+        }
+
+        [Authorize]
+        [HttpPut("DeActiveExam")]
+        public IActionResult DeActiveExam(List<int> listID)
+        {
+            try
+            {
+                var check = schedule.DeactiveExam(listID);
+                if (check)
+                {
+                    return Ok(new
+                    {
+                        Status = true
+                    });
+                }
+                else
+                {
+                    return Ok(new
+                    {
+                        Status = false
+                    });
+                }
+            }
+            catch
+            {
+                return Ok(new
+                {
+                    Status = false
+                });
+            }
+        }
+
+
+        [HttpPost("InsertExam")]
+        public IActionResult InsertExam([FromBody] ExamResponse T)
+        {
+
+            try
+            {
+                Account a = GetCurrentUser();
+                RcRequestExam tobj = new RcRequestExam();
+                tobj.Name = T.Name;
+                tobj.Status = -1;
+                tobj.Note = T.Note;
+                if (T.HeDiem != 0)
+                {
+                    tobj.HeDiem = T.HeDiem;
+                }
+                if (T.DiemQua != 0)
+                {
+                    tobj.DiemQua = T.DiemQua;
+                }
+                if (T.RequestId != 0)
+                {
+                    tobj.RequestId = T.RequestId;
+                }
+                tobj.CreateBy = a.Employee?.FullName;
+                var check = schedule.InsertExam(tobj);
+                return Ok(new
+                {
+                    Status = check
+                });
+
+            }
+            catch
+            {
+                return Ok(new
+                {
+                    Status = false
+                });
+            }
+        }
+
+        [HttpPost("ModifyExam")]
+        public IActionResult ModifyExam([FromBody] ExamResponse T)
+        {
+
+            try
+            {
+                Account a = GetCurrentUser();
+                RcRequestExam tobj = new RcRequestExam();
+                tobj.Id = T.Id;
+                tobj.Name = T.Name;
+                tobj.Status = -1;
+                tobj.Note = T.Note;
+                if (T.HeDiem != 0)
+                {
+                    tobj.HeDiem = T.HeDiem;
+                }
+                if (T.DiemQua != 0)
+                {
+                    tobj.DiemQua = T.DiemQua;
+                }
+                if (T.RequestId != 0)
+                {
+                    tobj.RequestId = T.RequestId;
+                }
+                tobj.UpdateBy = a.Employee?.FullName;
+                var check = schedule.ModifyExam(tobj);
+                return Ok(new
+                {
+                    Status = check
+                });
+
+            }
+            catch
+            {
+                return Ok(new
+                {
+                    Status = false
+                });
+            }
+        }
+
+        #endregion
+
 
     }
 }
---
+++ BE/Capstone/API/ResponseModel/Schedule/Schedule.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.ResponseModel.Schedule
+{
+    public class ScheduleResponse
+    {
+        public int Id { get; set; }
+        public int? IsInteview { get; set; }
+        public int? IsExam { get; set; }
+        public int? ExamId { get; set; }
+        public int? InterViewId { get; set; }
+        public DateTime? Date { get; set; }
+        public string DiaDiem { get; set; }
+        public int? HinhThuc { get; set; }
+        public int? CandidateId { get; set; }
+        public string Note { get; set; }
+        public int? ExpectedCost { get; set; }
+        public DateTime? StartHourExam { get; set; }
+        public DateTime? EndHourExam { get; set; }
+        public DateTime? DateNotify { get; set; }
+        public int? StatusContact { get; set; }
+        public DateTime? GioPv { get; set; }
+        public int? IdNguoiPv { get; set; }
+        public string CreateBy { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string UpdateBy { get; set; }
+        public DateTime UpdateDate { get; set; }
+    }
+    public class ExamResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public int? Status { get; set; }
+        public string Note { get; set; }
+        public int? HeDiem { get; set; }
+        public int? DiemQua { get; set; }
+        public int? RequestId { get; set; }
+        public string CreateBy { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public string UpdateBy { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+}
